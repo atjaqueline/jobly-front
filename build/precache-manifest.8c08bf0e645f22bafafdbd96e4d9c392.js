e7b5756bf601de40174c1ed508239a8191d150f6
jaquelinelin <88630180+jaquelinelin@users.noreply.github.com>
2023-02-18 15:24:35 -0500

    add files

---
+++ build/precache-manifest.8c08bf0e645f22bafafdbd96e4d9c392.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a463d6b3f468963293c42a683aba6b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15aff714c73288987314",
+    "url": "/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "47ce246a1cbf1672fa77",
+    "url": "/static/css/main.658eb3e8.chunk.css"
+  },
+  {
+    "revision": "15aff714c73288987314",
+    "url": "/static/js/2.3ab79649.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.3ab79649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ce246a1cbf1672fa77",
+    "url": "/static/js/main.0500e797.chunk.js"
+  },
+  {
+    "revision": "50b8b38e359d53208b9e",
+    "url": "/static/js/runtime-main.8a240d35.js"
+  }
+]);
